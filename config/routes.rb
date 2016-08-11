cb5e04fdbe7080aae8d3c9fcba6c1ccaf4f27bbf
Naty <matykowska.natalia@gmail.com>
2016-08-11 10:57:14 +0200

    solve authentication issues; add static pages and include links in navi bar

--- config/routes.rb
+++ config/routes.rb
@@ -4,4 +4,8 @@ Rails.application.routes.draw do
   end
 
   root 'entries#index'
+
+  match '/about', to: 'pages#about', via: [:get, :post]
+  match '/pictures', to: 'pages#pictures', via: [:get, :post]
+  match '/contact', to: 'pages#contact', via: [:get, :post]
 end
