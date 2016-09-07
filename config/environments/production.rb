50577f880105b62956ee1f4b12fae910069bb07d
Naty <matykowska.natalia@gmail.com>
2016-09-07 14:32:17 +0200

    fix in production.rb file;smtp fix

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -85,13 +85,13 @@ Rails.application.configure do
 	config.action_mailer.raise_delivery_errors = true
   config.action_mailer.perform_deliveries = true
 
-  config.action_mailer.default_url_options = { :host => 'https://codecouple.herokuapp.com' }
+  config.action_mailer.default_url_options = { :host => 'codecouple.herokuapp.com' }
 
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.smtp_settings = {
     :address => "smtp.gmail.com",
     :port => "587",
-    :domain => "https://codecouple.herokuapp.com",
+    :domain => "codecouple.herokuapp.com",
     :user_name => "info.codecouple@gmail.com",
     :password => "monkeysareannoying",
     :authentication => "plain",
