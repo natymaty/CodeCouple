8e9aaee671dab7aa66bbeae9b562f709fbcaf9ec
Naty <matykowska.natalia@gmail.com>
2016-09-13 11:01:52 +0200

    sendgrid config for production

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -89,11 +89,11 @@ Rails.application.configure do
 
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.smtp_settings = {
-    :address => "smtp.gmail.com",
+    :address => "smtp.sendgrid.net",
     :port => "587",
     :domain => "heroku.com",
-    :user_name => "info.codecouple@gmail.com",
-    :password => "monkeysareannoying",
+    :user_name => ENV['SENDGRID_USERNAME'],
+    :password => ENV['SENDGRID_PASSWORD'],
     :authentication => "plain",
     :enable_starttls_auto => true
   }
